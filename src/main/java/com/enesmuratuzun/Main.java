407b4c1b72bf9ba8c1af9897044013e7c7eb57c5
enesuzun2002 <emurat.uzun@gmail.com>
2022-09-23 20:41:33 +0300

    pdf-editor: add Apache PDF Box 3.0.0 alpha3

--- src/main/java/com/enesmuratuzun/Main.java
+++ src/main/java/com/enesmuratuzun/Main.java
@@ -1,7 +1,9 @@
 package com.enesmuratuzun;
 
+import com.enesmuratuzun.view.PDFInformation;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PDFInformation.main(null);
     }
 }
