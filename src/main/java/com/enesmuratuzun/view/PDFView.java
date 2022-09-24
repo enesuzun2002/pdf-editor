79649dad6519182ebfad528f1f3d4e3900135217
enesuzun2002 <emurat.uzun@gmail.com>
2022-09-24 11:26:54 +0300

    pdf-editor: add functionality to save pdf file as images

--- src/main/java/com/enesmuratuzun/view/PDFView.java
+++ src/main/java/com/enesmuratuzun/view/PDFView.java
@@ -1,12 +1,17 @@
 package com.enesmuratuzun.view;
 
 import com.enesmuratuzun.model.Preferences;
+import com.enesmuratuzun.util.PDFFunctions;
 import com.enesmuratuzun.util.SwingFunctions;
+import com.enesmuratuzun.view.dialog.PDFNull;
 import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PDFView {
     private JList list1;
@@ -25,15 +30,16 @@ public class PDFView {
 
     private static JMenuBar setMenuBar() {
         JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = createFileMenu();
-        JMenu fileInfoMenu = createFileInfoMenu();
+        JMenu fileMenu = getFileMenu();
+        JMenu fileInfoMenu = getFileInfoMenu();
         menuBar.add(fileMenu);
         menuBar.add(fileInfoMenu);
         return menuBar;
     }
 
-    private static JMenu createFileMenu() {
+    private static JMenu getFileMenu() {
         JMenu fileMenu = new JMenu("File");
+        JMenu fileSaveAsMenu = getFileSaveAsMenu();
         JMenuItem fileChooseMenuItem = SwingFunctions.createMenuItem("Open", "Open file", e -> {
             JFileChooser jfc = new JFileChooser();
             jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -58,19 +64,47 @@ public class PDFView {
         });
         fileMenu.add(fileChooseMenuItem);
         fileMenu.add(fileSaveMenuItem);
+        fileMenu.add(fileSaveAsMenu);
         return fileMenu;
     }
 
-    private static JMenu createFileInfoMenu() {
+    private static JMenu getFileInfoMenu() {
         JMenu fileInfoMenu = new JMenu("About");
         JMenuItem infoEditorMenuItem = SwingFunctions.createMenuItem("Edit", "Edit Information", e -> {
             if (Preferences.document == null) {
-                JOptionPane.showMessageDialog(frame, "Please open a PDF file.");
+                PDFNull.main(null);
             } else {
                 PDFInformation.main(null);
             }
         });
         fileInfoMenu.add(infoEditorMenuItem);
         return fileInfoMenu;
+    }
+
+    private static JMenu getFileSaveAsMenu() {
+        JMenu fileSaveAsMenu = new JMenu("Save As..");
+        JMenuItem imagesMenuItem = SwingFunctions.createMenuItem("Save As Images", "Save modified pdf file as images", e -> {
+            if (Preferences.document != null) {
+                JFileChooser jfc = new JFileChooser();
+                jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int returnVal = jfc.showOpenDialog(frame);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = jfc.getSelectedFile();
+                    try {
+                        ArrayList<BufferedImage> bufferedImages = PDFFunctions.getPDFPageImages(Preferences.documentPDF, 1);
+                        for (int i = 0; i < bufferedImages.size(); i++) {
+                            ImageIOUtil.writeImage(bufferedImages.get(i), file.getPath() + "/" + Preferences.document.getName().replaceAll("\\.pdf", "-" + i + ".png"), 300);
+                        }
+
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            } else {
+                PDFNull.main(null);
+            }
+        });
+        fileSaveAsMenu.add(imagesMenuItem);
+        return fileSaveAsMenu;
     }
 }
